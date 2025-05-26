09a87c4d615b8e002082b864d68ab884a6dc0b65
Nikolay Sharapov <snn1208@yandex.ru>
2025-05-26 20:18:52 +0300

    11

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,4 @@ def client():
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.data == b'Hello,People!'
